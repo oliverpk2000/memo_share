3c4b49cfba32f9e7392924778276dcd71fa63bb8
oliver <oliver.pecek@gmail.com>
2023-12-28 19:22:17 +0100

    wrote entry.dart

--- lib/domain/entry.dart
+++ lib/domain/entry.dart
@@ -1,3 +1,10 @@
 class Entry{
+  late int id;
+  late String title;
+  late String content;
+  late List<String> tags;
+  late bool private;
+  late List<String> imageUrls;
 
+  Entry(this.id, this.title, this.content, this.tags, this.private, this.imageUrls);
 }
