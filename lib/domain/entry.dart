548635f954505db9db999212adfb2ed9d15984ba
Roman <roman.mohnl@htlstp.at>
2023-12-29 21:14:54 +0100

    Interface improvements and Get/Put methods

--- lib/domain/entry.dart
+++ lib/domain/entry.dart
@@ -1,4 +1,4 @@
-class Entry{
+class Entry {
   late int id;
   late String title;
   late String content;
@@ -6,5 +6,55 @@ class Entry{
   late bool private;
   late List<String> imageUrls;
 
-  Entry(this.id, this.title, this.content, this.tags, this.private, this.imageUrls);
+  static int idCount = 1;
+
+  Entry({required this.title, required this.content, required this.tags, required this.private, required this.imageUrls}) {
+    id = idCount;
+    idCount++;
+  }
+
+  Entry.includeId({required this.id, required this.title, required this.content, required this.tags, required this.private, required this.imageUrls});
+
+
+  factory Entry.fromJSON(Map<String, dynamic> jsonMap) {
+    return Entry.includeId(id: jsonMap['id'],
+        title: jsonMap['title'],
+        content: jsonMap['content'],
+        tags: jsonMap['tags'],
+        private: jsonMap['private'],
+        imageUrls: jsonMap['imageUrls']
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id' : id,
+      'title' : title,
+      'content' : content,
+      'tags' : tags,
+      'private': private,
+      'imageUrls': imageUrls
+    };
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    var entry = other as Entry;
+    return entry.id == id;
+  }
+
+  @override
+  String toString() {
+    return """{
+      id : $id,
+      title : $title,
+      content : $content,
+      tags : $tags,
+      private: $private,
+      imageUrls: $imageUrls
+    }""";
+  }
 }
---
+++ lib/services/EntryService.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:memo_share/domain/entry.dart';
+
+class EntryService {
+  final String pathToDB =
+      "https://memoshare-bde3c-default-rtdb.europe-west1.firebasedatabase.app/";
+
+  Future<void> addEntry(Entry entry) async {
+    var url = Uri.parse("$pathToDB/entries/${entry.id}.json");
+
+    try {
+      var response = await http.put(
+          url,
+          body: json.encode(entry.toJson()),
+      );
+
+      if (response.statusCode == 200) {
+        print("New Entry successfully created!");
+
+      } else {
+        throw "Something went wrong: Statuscode: ${response.statusCode}, Message: ${response.body}";
+      }
+    } catch (error) {
+      throw "Error while creating: $error";
+    }
+  }
+
+  Future<void> addEntries(List<Entry> entries) async {
+    for (var entry in entries) {
+      await addEntry(entry);
+    }
+  }
+
+  Future<List<Entry>> getAll() async {
+    List<Entry> entries = [];
+    var url = Uri.parse("$pathToDB/entries.json");
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      data.forEach((key, value) {
+        entries.add(Entry.fromJSON(value));
+      });
+
+      return entries;
+
+    } else {
+      throw "Something went wrong while getting all Entries";
+
+    }
+  }
+
+  Future<Entry> getEntry(int id) async {
+    var url = Uri.parse("$pathToDB/entries/$id.json");
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return Entry.fromJSON(json.decode(response.body));
+
+    } else {
+      throw "Something went wrong while searching for ID: $id";
+    }
+  }
+}
