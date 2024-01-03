567e7b00f0c3f1afebe23c44ad1f2816b59e1d68
Roman <roman.mohnl@htlstp.at>
2024-01-03 18:52:56 +0100

    Changing Methods finished

--- lib/services/UserService.dart
+++ lib/services/UserService.dart
@@ -21,7 +21,6 @@ class UserService {
       }
 
       return users;
-
     } else {
       throw "Somwthing went wrong while getting all Users";
     }
@@ -43,11 +42,14 @@ class UserService {
     }
   }
 
-  Future<void> registerUser(User user) async {
+  Future<void> registerUser(User user, {bool checkDoubleNames = true}) async {
     var url = Uri.parse("$pathToDB/users/${user.id}.json");
     var allUsers = await getAll();
 
-    if (allUsers.where((element) => element.username == user.username).toList().isNotEmpty) {
+    if (checkDoubleNames && allUsers
+        .where((element) => element.username == user.username)
+        .toList()
+        .isNotEmpty) {
       throw "Username: ${user.username} already exists";
     }
 
@@ -58,7 +60,6 @@ class UserService {
 
       if (response.statusCode == 200) {
         print("User Successfully Created");
-
       } else {
         throw "Something went wrong whlie creating User: $user";
       }
@@ -69,11 +70,11 @@ class UserService {
 
   Future<User> login(User user) async {
     var allUsers = await getAll();
-    var withUsername = allUsers.where((element) => element.username == user.username);
+    var withUsername = allUsers.where((element) =>
+    element.username == user.username);
 
     if (withUsername.isEmpty) {
       throw "No user with this name: ${user.username}";
-
     } else if (withUsername.first.password == user.password) {
       return withUsername.first;
     }
@@ -84,12 +85,54 @@ class UserService {
   Future<void> addToCreated(entryId, userId) async {
     try {
       var user = await getUser(userId);
+      user.created.add(entryId);
+      await registerUser(user,
+          checkDoubleNames: false); // If anything except USername changes we dont have to check if there are duplicates
 
     } catch (error) {
-
+      throw "User not found: $userId";
     }
   }
 
+  Future<void> addToLiked(entryId, userId) async {
+    try {
+      var user = await getUser(userId);
+      user.liked.add(entryId);
+      await registerUser(user, checkDoubleNames: false);
+    } catch (error) {
+      throw "User not found: $userId";
+    }
+  }
 
-    //TODO created, liked, favorite
+  Future<void> addToFavorite(int entryId, int userId) async {
+    try {
+      var user = await getUser(userId);
+      user.favorited.add(entryId);
+      await registerUser(user, checkDoubleNames: false);
+    } catch (error) {
+      throw "User not found: $userId";
+    }
+  }
+
+  Future<void> changeUsername(String username, int userId ) async {
+    try {
+      var user = await getUser(userId);
+      user.username = username;
+      await registerUser(user);
+
+    } catch (error) {
+      throw "User $userId not found";
+    }
+  }
+
+  Future<void> changePassword(String password, int userId ) async {
+    try {
+      var user = await getUser(userId);
+      user.password = password;
+      await registerUser(user, checkDoubleNames: false);
+
+    } catch (error) {
+      throw "User $userId not found";
+    }
+  }
 }
